96002c6bced48a6d036db337eb45be22fd1edf38
Tamiakhan <tamiakhan017@gmail.com>
2026-03-10 15:50:53 +0600

    Update ArrivalOfTheGenral.cpp

--- ArrivalOfTheGenral.cpp
+++ ArrivalOfTheGenral.cpp
@@ -1,1 +1,33 @@
+//https://codeforces.com/problemset/problem/144/A
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int max_val = 0, min_val = 101;
+    int max_idx = 0, min_idx = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if (a[i] > max_val) {
+            max_val = a[i];
+            max_idx = i;
+        }
+        if (a[i] <= min_val) { 
+            min_val = a[i];
+            min_idx = i;
+        }
+    }
+
+    int ans = max_idx + (n - 1 - min_idx);
+    if (max_idx > min_idx) ans--; 
+    
+    cout << ans << endl;
+    return 0;
+}
 
