d1acad4fe6d4a9c2191528f8dbddccfc0fbe4f04
Tamiakhan <tamiakhan017@gmail.com>
2026-03-10 15:19:03 +0600

    Update A+B.cpp

--- A+B.cpp
+++ A+B.cpp
@@ -1,1 +1,17 @@
+https://codeforces.com/problemset/problem/1772/A
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        int a = s[0] - '0';
+        int b = s[2] - '0';
+        cout << a + b << endl;
+    }
+    return 0;
+}
 
