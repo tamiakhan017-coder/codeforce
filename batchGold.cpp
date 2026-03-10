a26f4863d27acaa42ca212f6030160672739a9f8
Tamiakhan <tamiakhan017@gmail.com>
2026-03-10 15:53:52 +0600

    Update batchGold.cpp
    Added a solution for problem 1829/D from Codeforces.

--- batchGold.cpp
+++ batchGold.cpp
@@ -1,1 +1,12 @@
+//https://codeforces.com/problemset/problem/1829/D
+#include <iostream>
+using namespace std;
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        string s; cin >> s;
+        cout << (s[0] - '0') + (s[2] - '0') << endl;
+    }
+}
+
 
