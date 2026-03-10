a8829a834cf6f24eea5503ee05330392826f6eb3
Tamiakhan <tamiakhan017@gmail.com>
2026-03-10 15:36:14 +0600

    Update AntonNDLetters.cpp

--- AntonNDLetters.cpp
+++ AntonNDLetters.cpp
@@ -1,1 +1,23 @@
+//https://codeforces.com/problemset/problem/443/A
+#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+
+int main() 
+{
+    string s;
+    getline(cin, s); 
+    set<char> distinct_letters;
+    
+    for (char c : s) {
+        if (c >= 'a' && c <= 'z') {
+            distinct_letters.insert(c);
+        }
+    }
+    
+    cout << distinct_letters.size() << endl;
+    return 0;
+}
+
 
