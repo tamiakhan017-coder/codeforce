903bfb38602ba43a06eccc77f12b38df509c021b
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 21:35:58 +0600

    Update ReminderRequired.cpp

--- ReminderRequired.cpp
+++ ReminderRequired.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    long long n, k;
+    cin >> n >> k;
+    if (k > 42) {
+        cout << "No" << endl;
+        return 0;
+    }
+
+    set<long long> s;
+    for (long long i = 1; i <= k; i++) {
+        if (s.count(n % i)) {
+            cout << "No" << endl;
+            return 0;
+        }
+        s.insert(n % i);
+    }
+
+    cout << "Yes" << endl;
+    return 0;
+}
 
