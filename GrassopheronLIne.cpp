4fe4b79f8a0831999ad485e1349c1b8fbbbba9b4
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 20:12:56 +0600

    Update GrassopheronLIne.cpp
    Add a solution function to handle input and output based on divisibility.

--- GrassopheronLIne.cpp
+++ GrassopheronLIne.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    int x, k;
+    cin >> x >> k;
+
+    if (x % k != 0) {
+        cout << 1 << endl;
+        cout << x << endl;
+    } else {
+        cout << 2 << endl;
+        cout << x - 1 << " " << 1 << endl;
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
 
