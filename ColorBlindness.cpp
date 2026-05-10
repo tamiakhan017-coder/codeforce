b2061a123d333846af79c4c1e9a999afccf7e3ca
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 18:34:29 +0600

    Update ColorBlindness.cpp

--- ColorBlindness.cpp
+++ ColorBlindness.cpp
@@ -1,1 +1,24 @@
+//https://codeforces.com/problemset/problem/432/A
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    if (!(cin >> n >> k)) return 0;
+
+    int eligibleCount = 0;
+    for (int i = 0; i < n; i++) {
+        int y;
+        cin >> y;
+        if (y + k <= 5) {
+            eligibleCount++;
+        }
+    }
+    cout << eligibleCount / 3 << endl;
+
+    return 0;
+}
+
 
