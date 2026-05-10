bdf142c90d8cb213a139f24487e6812a04f58751
Tamiakhan <tamiakhan017@gmail.com>
2026-05-11 02:06:39 +0600

    Update SortSubstring.cpp
    Add functionality to sort strings by length and check substring presence.

--- SortSubstring.cpp
+++ SortSubstring.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <vector>
+
+#include <algorithm>
+
+using namespace std;
+
+// Custom comparator to sort strings by length
+bool compareByLength(const string &a, const string &b) {
+    return a.length() < b.length();
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<string> s(n);
+    for (int i = 0; i < n; i++) {
+        cin >> s[i];
+    }
+    sort(s.begin(), s.end(), compareByLength);
+
+    for (int i = 0; i < n - 1; i++) {
+        if (s[i+1].find(s[i]) == string::npos) {
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+    cout << "YES" << endl;
+    for (const string &str : s) {
+        cout << str << endl;
+    }
+
+    return 0;
+}
 
