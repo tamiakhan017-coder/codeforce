cbdc3649a4ddc6a138680dd3bc959643797b5cac
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 18:19:16 +0600

    Update canISquare.cpp

--- canISquare.cpp
+++ canISquare.cpp
@@ -1,1 +1,22 @@
+//https://codeforces.com/contest/1915/problem/C
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long n, ans;
+    if (!(cin >> n))
+        return 0;
+    if (n % 2 == 0) {
+        ans = n / 2;
+    } else {
+        ans = -(n + 1) / 2;
+    }
+
+    long long squaredAns = ans * ans;
+    cout << squaredAns << endl;
+
+    return 0;
+}
+
 
