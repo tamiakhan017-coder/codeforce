7d455b1ba82357b3ebcd39b53188ffaa6a0c19ff
Tamiakhan <tamiakhan017@gmail.com>
2026-05-11 02:09:01 +0600

    Implement solution for Spy Detection problem

--- SpyDetected.cpp
+++ SpyDetected.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    if (a[0] != a[1] && a[0] != a[2]) {
+        cout << 1 << endl;
+    } else if (a[1] != a[0] && a[1] != a[2]) {
+        cout << 2 << endl;
+    } else {
+       
+        for (int i = 2; i < n; i++) {
+            if (a[i] != a[0]) {
+                cout << i + 1 << endl;
+                break;
+            }
+        }
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
 
