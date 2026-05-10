fa9665c5b6ec021b34ed667ae632ce9e5d13c4e4
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 18:10:37 +0600

    Update calculatingFunction.cpp
    Implement main function to calculate result based on even or odd input.

--- calculatingFunction.cpp
+++ calculatingFunction.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n; // Read input n
+
+    if (n % 2 == 0) {
+        // If n is even, the result is n/2
+        cout << n / 2 << endl;
+    } else {
+        // If n is odd, the result is -(n+1)/2
+        cout << -(n + 1) / 2 << endl;
+    }
+
+    return 0;
+}
 
