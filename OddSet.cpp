a12f8c08e41069c02ce45d216e3bebb184a3bc49
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 20:55:40 +0600

    Update OddSet.cpp

--- OddSet.cpp
+++ OddSet.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    int oddCount = 0;
+    
+    // We check 2*n numbers
+    for (int i = 0; i < 2 * n; i++) {
+        int x;
+        cin >> x;
+        if (x % 2 != 0) oddCount++;
+    }
+
+    // If half the numbers are odd, we can form n pairs
+    if (oddCount == n) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
 
