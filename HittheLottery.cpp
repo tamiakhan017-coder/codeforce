585c190e64cef20a92f1af19358ea054fe38bc51
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 20:15:59 +0600

    Update HittheLottery.cpp

--- HittheLottery.cpp
+++ HittheLottery.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int bills[] = {100, 20, 10, 5, 1};
+    int count = 0;
+
+    for (int i = 0; i < 5; i++) {
+        count += n / bills[i];
+        n %= bills[i];
+    }
+
+    cout << count << endl;
+    return 0;
+}
 
