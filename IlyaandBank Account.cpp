357904e50f24c3c5e213d92f2009249b742f811b
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 20:21:08 +0600

    Update IlyaandBank Account.cpp

--- IlyaandBank Account.cpp
+++ IlyaandBank Account.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n >= 0) {
+        cout << n << endl;
+    } else {
+        int opt1 = n / 10;
+        int opt2 = (n / 100) * 10 + (n % 10);
+        cout << max({n, opt1, opt2}) << endl;
+    }
+
+    return 0;
+}
 
