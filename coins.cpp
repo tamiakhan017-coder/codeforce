9055cb84ca7721ab76178c069de5a9fcd1f27437
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 18:41:00 +0600

    Update coins.cpp
    Implement coin distribution calculation based on input values.

--- coins.cpp
+++ coins.cpp
@@ -1,1 +1,17 @@
+//https://codeforces.com/problemset/problem/1061/A
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, s;
+    cin >> n >> s;
+
+    // Use integer division; if there's a remainder, add one more coin
+    if (s % n == 0) {
+        cout << s / n << endl;
+    } else {
+        cout << (s / n) + 1 << endl;
+    }
+    return 0;
+}
 
