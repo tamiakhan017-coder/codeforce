d375f10fac383a4662e9fa1309d39e067102c4c6
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 21:38:38 +0600

    Implement solution to remove smallest element

--- RemoveSmallest.cpp
+++ RemoveSmallest.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    sort(a.begin(), a.end());
+
+    bool possible = true;
+    for (int i = 1; i < n; i++) {
+        if (a[i] - a[i - 1] > 1) {
+            possible = false;
+            break;
+        }
+    }
+
+    if (possible) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
 
