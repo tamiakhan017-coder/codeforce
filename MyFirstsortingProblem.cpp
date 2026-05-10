456f9db716cc8349476d213fccf52c62bf9c137e
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 20:48:16 +0600

    Update MyFirstsortingProblem.cpp

--- MyFirstsortingProblem.cpp
+++ MyFirstsortingProblem.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    int x, y;
+    cin >> x >> y;
+
+    cout << min(x, y) << " " << max(x, y) << endl;
+}
+
+int main() {
+    int t;
+    cin >> t; 
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
 
