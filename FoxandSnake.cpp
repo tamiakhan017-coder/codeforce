13022ec24f08bbfe9c0b820f1fe4a0601212a3c0
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 19:07:01 +0600

    Update FoxandSnake.cpp
    Implement Fox and Snake pattern output based on input dimensions.

--- FoxandSnake.cpp
+++ FoxandSnake.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 != 0) {
+            for (int j = 0; j < m; j++) cout << "#";
+        } else {
+            if ((i / 2) % 2 != 0) {
+                for (int j = 0; j < m - 1; j++) cout << ".";
+                cout << "#";
+            } else {
+                cout << "#";
+                for (int j = 0; j < m - 1; j++) cout << ".";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
 
