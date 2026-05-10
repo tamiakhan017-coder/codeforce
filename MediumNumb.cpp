80be9b4fe83dbc233ec7e92c94292c04797ee863
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 20:34:49 +0600

    Update MediumNumb.cpp
    Implement function to find the median of three numbers

--- MediumNumb.cpp
+++ MediumNumb.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    vector<int> a(3);
+    for (int i = 0; i < 3; i++) cin >> a[i];
+    sort(a.begin(), a.end());
+    cout << a[1] << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
 
