86885c26f9bd2721d2136dcc268bfa09231d0e99
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 18:46:37 +0600

    Update CreatingWords.cpp

--- CreatingWords.cpp
+++ CreatingWords.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <algorithm> 
+
+using namespace std;
+
+int main() {
+    int t;
+    if (!(cin >> t)) 
+        return 0;
+
+    while (t--) {
+        string a, b;
+        cin >> a >> b;
+        swap(a[0], b[0]);
+        cout << a << " " << b << endl;
+    }
+
+    return 0;
+}
 
