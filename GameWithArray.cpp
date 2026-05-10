2ae11dadedabe41cd29d2009f4b6038eff5b36c9
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 20:07:53 +0600

    Add game logic to check sum conditions
    Implement a game logic that checks if the sum can be achieved with given conditions.

--- GameWithArray.cpp
+++ GameWithArray.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, s;
+    cin >> n >> s;
+
+    if (s >= 2 * n) {
+        cout << "YES" << endl;
+        for (int i = 0; i < n - 1; i++) {
+            cout << 1 << " ";
+        }
+        cout << s - (n - 1) << endl;
+        cout << n << endl; 
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
 
