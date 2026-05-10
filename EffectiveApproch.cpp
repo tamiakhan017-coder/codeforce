87e1147083d735d0913895d2aaf685a63f888afa
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 19:01:43 +0600

    Update EffectiveApproch.cpp

--- EffectiveApproch.cpp
+++ EffectiveApproch.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector<int> pos(n + 1);
+    for (int i = 1; i <= n; i++) {
+        int val;
+        cin >> val;
+        pos[val] = i; 
+    }
+
+    int m;
+    cin >> m;
+    long long vasya = 0, petya = 0;
+
+    while (m--) {
+        int query;
+        cin >> query;
+        vasya += pos[query];
+        petya += (n - pos[query] + 1);
+    }
+
+    cout << vasya << " " << petya << endl;
+
+    return 0;
+}
 
