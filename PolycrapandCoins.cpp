ae45b42121496d99087bae6d3588a0b06a8c52a5
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 21:30:18 +0600

    Update PolycrapandCoins.cpp
    Added a solution function to process input and calculate values based on the input number.

--- PolycrapandCoins.cpp
+++ PolycrapandCoins.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    int q = n / 3;
+    int r = n % 3;
+
+    int c1 = q, c2 = q;
+
+    if (r == 1) {
+        c1++;
+    } else if (r == 2) {
+        c2++;
+    }
+
+    cout << c1 << " " << c2 << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
 
