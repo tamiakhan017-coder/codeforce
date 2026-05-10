c6d81e6506f9066f0b2a2823578ef566dfab7e3d
Tamiakhan <tamiakhan017@gmail.com>
2026-05-10 20:30:36 +0600

    Update LoveStory.cpp

--- LoveStory.cpp
+++ LoveStory.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve() {
+    string target = "codeforces";
+    string s;
+    cin >> s;
+
+    int diff = 0;
+    for (int i = 0; i < 10; i++) {
+        if (s[i] != target[i]) {
+            diff++;
+        }
+    }
+    cout << diff << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
+
 
