15f8da874dcc8bd83d72e257e76dc93abf3540ca
Tamiakhan <tamiakhan017@gmail.com>
2026-05-11 07:29:41 +0600

    Update george.cpp
    Add a program to count pairs with a difference of at least 2.

--- george.cpp
+++ george.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, p, q, count = 0;
+    cin >> n;
+    while (n--) {
+        cin >> p >> q;
+        if (q - p >= 2) {
+            count++;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
 
