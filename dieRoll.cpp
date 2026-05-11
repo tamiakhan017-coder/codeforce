b10b6193844f5c7d459de1de8cab9b5447a6a9eb
Tamiakhan <tamiakhan017@gmail.com>
2026-05-11 07:25:27 +0600

    Update dieRoll.cpp

--- dieRoll.cpp
+++ dieRoll.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int Y, W;
+    cin >> Y >> W;
+    int M = max(Y, W);
+    int A = 6 - M + 1;
+    
+   
+    string results[] = {"", "1/6", "1/3", "1/2", "2/3", "5/6", "1/1"};
+    cout << results[A] << endl;
+    
+    return 0;
+}
 
