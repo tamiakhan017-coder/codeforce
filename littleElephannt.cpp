10861c2f0c44624770498c626ad049ec0a3351b2
Tamiakhan <tamiakhan017@gmail.com>
2026-05-11 07:32:59 +0600

    Update littleElephannt.cpp
    Add logic to remove the first occurrence of '0' from the input string.

--- littleElephannt.cpp
+++ littleElephannt.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    
+    int n = s.length();
+    int zero_pos = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '0') {
+            zero_pos = i;
+            break;
+        }
+    }
+
+    if (zero_pos == -1) {
+        cout << s.substr(1) << endl;
+    } else {
+        cout << s.substr(0, zero_pos) << s.substr(zero_pos + 1) << endl;
+    }
+
+    return 0;
+}
 
