2cc7ab946774261e6318092c33e4eadf3ec12d31
Tamiakhan <tamiakhan017@gmail.com>
2026-05-11 07:22:39 +0600

    Update TrippiTroppi.cpp

--- TrippiTroppi.cpp
+++ TrippiTroppi.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string a, b, c;
+        cin >> a >> b >> c;
+        cout << a[0] << b[0] << c[0] << endl;
+    }
+    return 0;
+}
 
