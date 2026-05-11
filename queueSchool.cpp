95b59824caa9978f417548cbdbffd547f37bdd3f
Tamiakhan <tamiakhan017@gmail.com>
2026-05-11 07:49:12 +0600

    Update queueSchool.cpp

--- queueSchool.cpp
+++ queueSchool.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, t;
+    string s;
+    cin >> n >> t >> s;
+
+    while (t--) {
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == 'B' && s[i+1] == 'G') {
+                swap(s[i], s[i+1]);
+                i++; // Skip to prevent double movement
+            }
+        }
+    }
+
+    cout << s << endl;
+    return 0;
+}
 
