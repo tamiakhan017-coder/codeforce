b4b74572ab4e1a64449286909b5460dfc36ecf06
Tamiakhan <tamiakhan017@gmail.com>
2026-05-11 07:38:03 +0600

    Update newYear,cpp.c

--- newYear,cpp.c
+++ newYear,cpp.c
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int x[3];
+    cin >> x[0] >> x[1] >> x[2];
+    sort(x, x + 3);
+    cout << x[2] - x[0] << endl;
+    return 0;
+}
 
